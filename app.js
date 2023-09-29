85499d10095ee5641e67f37b5ec21b3491436e17
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-09-29 20:21:52 +0530

    completed tasks

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const dotenv = require('dotenv');
 dotenv.config();
 const sendsms = require("./routes/index");
-// const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
@@ -11,21 +10,7 @@ app.use(sendsms);
 app.use(cors());
 
 
-// const User = require("./models/users");
-// mongoose.set("strictQuery", false);
-// mongoose
-//   .connect("mongodb://localhost:27017/VibtreeTask", {
-//     useNewUrlParser: true,
-//     // useFindAndModify: false,
-//     useUnifiedTopology: true,
-//   })
-//   .then((result) => {
-//     app.listen(5000);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-//start the server
+
 app.listen(5000, () => {
   console.log("app started at 5000");
 });
