4eae8879e0a2b15edbd323f507c649957f1a2aec
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-09-29 18:20:56 +0530

    package added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const sendsms = require("./routes/route");
+const sendsms = require("./routes/index");
 
 const app = express();
 
